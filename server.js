8a44d13a7a93801133f7fb7b1b3f063f7c5afa29
SaikatKarar <sakatkarar01@gmail.com>
2026-07-23 13:26:54 +0530

    Remove server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-import { create } from "json-server";
-
-const server = await create();
-
-const PORT = process.env.PORT || 10000;
-
-server.listen(PORT, () => {
-    console.log(`JSON Server running on ${PORT}`);
-});
